e396169dbf2304a341199b8352de3dd159cf3990
Pakkapon Phongthawee <me@pureapp.in.th>
2017-07-07 20:17:01 +0700

    update very simple clock

---
+++ src/components/CurrentTime.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    {{dateText}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CurrentTime',
+  data () {
+    return {
+      date: new Date(),
+      dateText: '-'
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      this.updateDate()
+    }, 1000)
+    return
+  },
+  methods: {
+    zeroGap (num) {
+      if (num >= 10) {
+        return '' + num
+      } else {
+        return '0' + num
+      }
+    },
+    updateDate () {
+      this.date = new Date()
+      this.dateText = this.zeroGap(this.date.getHours()) + ':' + this.zeroGap(this.date.getMinutes()) + ':' + this.zeroGap(this.date.getSeconds())
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
